dcafa922729e8b0a75eac11de693a69a22a9f641
candacecodes <candacehchoe@gmail.com>
2020-11-19 14:48:05 -0800

    bootstrap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/home/Login";
 import Navbar from "./components/Navbar";
+import Home from "./components/home/Home";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<Navbar />
+				<Home />
 			</header>
 		</div>
 	);
