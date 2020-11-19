c82e10303e7be289c5461b2045130d09c3ac7056
candacecodes <candacehchoe@gmail.com>
2020-11-19 15:07:50 -0800

    rough draft home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
 
 export default function Home() {
-	return <div>Home Banner</div>;
+	return (
+		<div>
+			<Jumbotron fluid className="banner">
+				<Container>
+					<h1>Rx Roundtable</h1>
+					<p>
+						<b>Rx: A Doctor's Prescription</b>
+						<br />
+						<br />
+						<i>
+							The "R" in "Rx" stands for the Latin word recipe, meaning "take,"
+							and the first doctor to use "Rx" used it as a verb with the same
+							meaning, "Rx two aspirin" being equivalent to today's "Take two
+							aspirin."
+						</i>
+					</p>
+				</Container>
+			</Jumbotron>
+		</div>
+	);
 }
