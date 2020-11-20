b56481c26636eef0b2812de2a7ea6685355a6384
candacecodes <candacehchoe@gmail.com>
2020-11-20 15:22:00 -0800

    rx container

--- src/components/FilterSearch.js
+++ src/components/FilterSearch.js
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import RxContainer from "../containers/RxContainer";
 
 export default class FilterSearch extends Component {
 	render() {
 		return (
-			<div>
-				<Form>
-					{/* <Form.Group controlId="exampleForm.ControlSelect1">
-						<Form.Label>Sort By</Form.Label>
-						<Form.Control as="select">
-							<option>Rx type 1</option>
-							<option>Rx type 2</option>
-							<option>Rx type 3</option>
-							<option>Rx type 4</option>
-							<option>Rx type 5</option>
-						</Form.Control>
-						<Button variant="outline-secondary">Search</Button>
-					</Form.Group> */}
-
-					<Form.Group controlId="exampleForm.ControlTextarea1">
-						<Form.Label>Search</Form.Label>
-						<Form.Control as="textarea" rows={1} />
-						<Button variant="outline-secondary">Search</Button>
-					</Form.Group>
-				</Form>
-			</div>
+			<>
+				<div>
+					<Form>
+						<Form.Group controlId="exampleForm.ControlTextarea1">
+							<Form.Label>Search Medications </Form.Label>
+							<Form.Control as="textarea" rows={1} />
+							<Button variant="outline-secondary">Search</Button>
+						</Form.Group>
+					</Form>
+					<RxContainer />
+				</div>
+			</>
 		);
 	}
 }
--- src/containers/RxContainer.js
+++ src/containers/RxContainer.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 
 export default class RxContainer extends Component {
-	renderConditions = () => {
-		return this.props.rxs.map((rx) => {
-			return (
-				<div>
-					<span data-toggle="collapse" data-target={rx.id}>
-						<div className="row">
-							<div className="item-header">{rx.name}</div>
-						</div>
-					</span>
-					<div id={rx.id} className="collapse">
-						<RxInfo key={rx.id} rx={rx} saveRx={this.props.saveRx} />
-					</div>
-				</div>
-			);
-		});
-	};
+	// renderConditions = () => {
+	// 	return this.props.rxs.map((rx) => {
+	// 		return (
+	// 			<div>
+	// 				<span data-toggle="collapse" data-target={rx.id}>
+	// 					<div className="row">
+	// 						<div className="item-header">{rx.name}</div>
+	// 					</div>
+	// 				</span>
+	// 				<div id={rx.id} className="collapse">
+	// 					<RxInfo key={rx.id} rx={rx} saveRx={this.props.saveRx} />
+	// 				</div>
+	// 			</div>
+	// 		);
+	// 	});
+	// };
 	render() {
 		return (
 			<div>
-				<div className="header">Prescriptions</div>
-				<div>{this.renderRx()}</div>
+				<div className="header">Search Results</div>
+				{/* <div>{this.renderRx()}</div> */}
 			</div>
 		);
 	}
