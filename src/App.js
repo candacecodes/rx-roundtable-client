8082fa65b16f416e25bc1ef2591cc97004c50d9e
candacecodes <candacehchoe@gmail.com>
2020-11-22 12:32:25 -0800

    updated state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/home/Login";
@@ -10,11 +11,18 @@ import FilterSearch from "./components/FilterSearch";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 
-function App() {
-	return (
-		<Router>
-			<div className="App">
-				{/* <header className="App-header">
+class App extends Component {
+	state = {
+		auth: {
+			user: {},
+		},
+	};
+
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					{/* <header className="App-header">
 				<Navbar />
 				<Home />
 				<RxSingle />
@@ -22,15 +30,16 @@ function App() {
 				<Login />
 			</header> */}
 
-				<main id="page-wrap">
-					<Route exact path="/home" component={Home} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/rx" component={FilterSearch} />
-					<Route exact path="/profile" component={Profile} />
-				</main>
-			</div>
-		</Router>
-	);
+					<main id="page-wrap">
+						<Route exact path="/home" component={Home} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/rx" component={FilterSearch} />
+						<Route exact path="/profile" component={Profile} />
+					</main>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
