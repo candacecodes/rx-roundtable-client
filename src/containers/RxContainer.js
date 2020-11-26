191308d2919a097bc8992f163ace709f5eaef357
candacecodes <candacehchoe@gmail.com>
2020-11-25 21:56:00 -0800

    working on mapping results

--- src/containers/RxContainer.js
+++ src/containers/RxContainer.js
@@ -1,40 +1,22 @@
 import React, { Component } from "react";
+import RxSingle from "../components/RxSingle";
 
 export default class RxContainer extends Component {
-	// renderResults = () => {
-	// 	return this.props.rxs.map((rx) => {
-	// 		return (
-	// 			<div>
-	// 				<span data-toggle="collapse" data-target={rx.id}>
-	// 					<div className="row">
-	// 						<div className="item-header">{rx.name}</div>
-	// 					</div>
-	// 				</span>
-	// 				<div id={rx.id} className="collapse">
-	// 					<RxInfo key={rx.id} rx={rx} saveRx={this.props.saveRx} />
-	// 				</div>
-	// 			</div>
-	// 		);
-	// 	});
-	// };
-
-	// renderResults = () => {
-	// 	<div>{this.props.result["results"]["0"]["patient"]["drug"]["0"]}</div>;
-	// };
-
-	// search = () => {
-	// 	console.log(this.props.result);
-	// };
+	mapResults = () => {
+		// console.log(this.props.result);
+		// this.props.result.map((info) => console.log(info));
+	};
 	render() {
-		console.log(this.props.result);
-		console.log(this.props.search);
+		// console.log(this.props.result);
+		// console.log(this.props.search);
 		// console.log(this.state);
 		// map through and render
 
 		return (
 			<div>
 				<div className="header">Search results for {this.props.search}</div>
-				{/* <div>{this.props}</div> */}
+				{/* {console.log(this.props.result)} */}
+				{this.mapResults}
 			</div>
 		);
 	}
