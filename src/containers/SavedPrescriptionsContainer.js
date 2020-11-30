46956b1274ef7bca37b59e415e03fc21b2ea4435
candacecodes <candacehchoe@gmail.com>
2020-11-30 14:55:23 -0800

    notes

--- src/containers/SavedPrescriptionsContainer.js
+++ src/containers/SavedPrescriptionsContainer.js
@@ -5,12 +5,15 @@ export default class SavedPrescriptionsContainer extends Component {
 		rxes: [],
 	};
 	mapRx = () => {
-		console.log(this.props.rxes);
-		// return console.log(this.props.rxes);
-		// map through array
-		// return this.props.rxes.map((rx) => {
-		// 	console.log("test");
-		// });
+		// map through each element in the array
+		// display an individual div for each prescription
+		// i think it's rxUserSingle
+
+		// problem: trying to map through rxes but error saying this.props.rxes.map is not a function
+		// is this because it's not in an array? (not sure)
+		// do i need to make this into an array?
+		// this.props.rxes.map((rx) => console.log(rx));
+		console.log("mapRx");
 	};
 
 	render() {
