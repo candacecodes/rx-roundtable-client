55214ab5910aae0a6aa6d4ad2422872078b9cda5
candacecodes <candacehchoe@gmail.com>
2020-12-02 13:45:06 -0800

    comments container

--- src/components/SavedPrescriptionSingle.js
+++ src/components/SavedPrescriptionSingle.js
@@ -1,23 +1,35 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
-
+import CommentsContainer from "../containers/CommentsContainer";
 export default class SavedPrescriptionSingle extends Component {
+	state = {
+		seeComment: false,
+	};
+
 	handleDelete = () => {
 		this.props.deleteSavedRx(this.props.rx);
 		// console.log(this.props.rx);
 	};
 
-	handleComment = () => {
-		this.props.commentSavedRx();
+	handleSeeComment = () => {
+		this.setState((prevState) => ({
+			seeComment: !prevState.seeComment,
+		}));
+		console.log(this.state);
 	};
 	render() {
 		return (
 			<div>
 				<div>
 					<h6>{this.props.rx}</h6>
-					<Button onClick={this.handleComment} variant="outline-dark" size="sm">
-						Comment
+					<Button
+						onClick={this.handleSeeComment}
+						variant="outline-dark"
+						size="sm"
+					>
+						See Comments
 					</Button>{" "}
+					{this.state.seeComment ? <CommentsContainer /> : null}
 					<Button onClick={this.handleDelete} variant="outline-dark" size="sm">
 						Delete
 					</Button>
---
+++ src/containers/CommentsContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CommentsContainer extends Component {
+	render() {
+		return <div>Comments Container</div>;
+	}
+}
